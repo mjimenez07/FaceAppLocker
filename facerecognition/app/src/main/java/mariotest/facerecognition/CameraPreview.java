3efb5698171e80477b7f85290bac62fed753a0d8
Mario Jimenez <mariojose.0732@gmail.com>
2016-06-22 16:28:38 -0400

    Moving camera instance to main activity to implement picture call back from camera in the main file

--- facerecognition/app/src/main/java/mariotest/facerecognition/CameraPreview.java
+++ facerecognition/app/src/main/java/mariotest/facerecognition/CameraPreview.java
@@ -24,10 +24,11 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     private Camera mCamera;
     public static final int MEDIA_TYPE_IMAGE = 1;
 
-    public CameraPreview(Context context) {
+    public CameraPreview(Context context, Camera camera) {
         super(context);
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
+        mCamera = camera;
         mHolder = getHolder();
         mHolder.addCallback(this);
         // deprecated setting, but required on Android versions prior to 3.0
@@ -38,7 +39,6 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, now tell the camera where to draw the preview.
         try {
-            mCamera = getCameraInstance();
             mCamera.setPreviewDisplay(holder);
         } catch (IOException e) {
             Log.d(VIEW_LOG_TAG, "Error setting camera preview: " + e.getMessage());
@@ -109,97 +109,5 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
                 Log.d("No face detected", ":/");
             }
         }
-    }
-
-    /** Check if this device has a camera */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            Log.d("Success: ", "The device has a camera");
-            return true;
-        } else {
-            // no camera on this device
-            Log.d("Error: ", "The device doesn't has a camera");
-            return false;
-        }
-    }
-
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
-        Camera c = null;
-
-        try {
-            c = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-            Log.d("Error:", "The camera couldn't be opened");
-        }
-        return c; // returns null if camera is unavailable
-    }
-
-    //TODO: verify why the camera freeze after we call the takepicture callback;
-    public void callTakePicture() {
-        Log.d("button clicked", "True");
-        mCamera.takePicture(null, null, mPicture);
-    }
-
-    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
-
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-
-            if (pictureFile == null){
-                Log.d("", "Error creating media file, check storage permissions:");
-                return;
-            }
-
-            try {
-                FileOutputStream fos = new FileOutputStream(pictureFile);
-                fos.write(data);
-                fos.close();
-            } catch (FileNotFoundException e) {
-                Log.d("Error", "File not found: " + e.getMessage());
-            } catch (IOException e) {
-                Log.d("Error", "Error accessing file: " + e.getMessage());
-            }
-        }
-    };
-
-    /** Create a file Uri for saving an image or video */
-    private static Uri getOutputMediaFileUri(int type){
-        return Uri.fromFile(getOutputMediaFile(type));
-    }
-
-    /** Create a File for saving an image or video */
-    private static File getOutputMediaFile(int type){
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
-                Log.d("app", "failed to create directory");
-                return null;
-            }
-        }
-
-        // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-
-        if (type == MEDIA_TYPE_IMAGE){
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_"+ timeStamp + ".jpg");
-        } else {
-            return null;
-        }
-
-        return mediaFile;
     }
 }
--- facerecognition/app/src/main/java/mariotest/facerecognition/MainActivity.java
+++ facerecognition/app/src/main/java/mariotest/facerecognition/MainActivity.java
@@ -1,5 +1,8 @@
 package mariotest.facerecognition;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,14 +11,14 @@ import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
     private CameraPreview mPreview;
-
+    private Camera mCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mPreview  = new CameraPreview(this);
+        mCamera = getCameraInstance();
+        mPreview  = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_frame);
         preview.addView(mPreview);
     }
@@ -26,4 +29,29 @@ public class MainActivity extends AppCompatActivity {
         Log.d("TODO!:","A few errors on capture pictures callback");
     }
 
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            Log.d("Success: ", "The device has a camera");
+            return true;
+        } else {
+            // no camera on this device
+            Log.d("Error: ", "The device doesn't has a camera");
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+
+        try {
+            c = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+            Log.d("Error:", "The camera couldn't be opened");
+        }
+        return c; // returns null if camera is unavailable
+    }
 }
