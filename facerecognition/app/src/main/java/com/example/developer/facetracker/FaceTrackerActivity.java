bea72ad08e3e10f143da551486d8efa3ad9dcd77
Mario Jimenez <mariojose.0732@gmail.com>
2016-09-08 15:52:14 -0400

    Adding new project

---
+++ facerecognition/app/src/main/java/com/example/developer/facetracker/FaceTrackerActivity.java
@@ -0,0 +1,13 @@
+package com.example.developer.facetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FaceTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_tracker);
+    }
+}
