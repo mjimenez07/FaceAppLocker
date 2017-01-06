5b100a1bd13fdbf71913fc5138613089c1b06fd6
Mario Jimenez <mariojose.0732@gmail.com>
2017-01-06 17:34:34 -0400

    Adding spaces

--- facerecognition/app/src/main/java/com/example/developer/facetracker/ConfirmPinActivity.java
+++ facerecognition/app/src/main/java/com/example/developer/facetracker/ConfirmPinActivity.java
@@ -5,32 +5,32 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class ConfirmPinActivity extends AppCompatActivity {
-    public SharedPreferences mSharedPreference;
+    public SharedPreferences mSharedPreferences;
+    public String userPassword;
+    public EditText confirmPin;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm_pin);
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_confirm_pin );
         init();
     }
 
     private  void init() {
-        mSharedPreference = getSharedPreferences("UserPin", MODE_PRIVATE);
-        final String userPassword = mSharedPreference.getString("pin","");
-        final EditText confirmPin = (EditText) findViewById(R.id.confirm_pin);
+        mSharedPreferences = getSharedPreferences( "UserPin", MODE_PRIVATE );
+        userPassword = mSharedPreferences.getString( "pin", "" );
+        confirmPin = ( EditText ) findViewById( R.id.confirm_pin );
 
-        confirmPin.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        confirmPin.setOnEditorActionListener( new TextView.OnEditorActionListener() {
             @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if (i == EditorInfo.IME_ACTION_DONE) {
-                    if (confirmPin.getText().toString().equals(userPassword)){
+            public boolean onEditorAction( TextView textView, int i, KeyEvent keyEvent ) {
+                if ( i == EditorInfo.IME_ACTION_DONE ) {
+                    if ( confirmPin() ) {
                         startFaceTrackerActivity();
                     }
                 }
@@ -39,10 +39,14 @@ public class ConfirmPinActivity extends AppCompatActivity {
         });
     }
 
+    private boolean confirmPin() {
+        return confirmPin.getText().toString().equals( userPassword );
+    }
+
     private void startFaceTrackerActivity() {
-        Intent intent = new Intent(getApplicationContext(), FaceTrackerActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+        Intent intent = new Intent( getApplicationContext(), FaceTrackerActivity.class );
+        intent.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
+        startActivity( intent );
         finish();
     }
 }
